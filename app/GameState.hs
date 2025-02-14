eb6bbd09e138e7a0a737f7d267d61a338512cd2e
Lenner Coutinho Pereira <lenner.coutinho.pereira@ccc.ufcg.edu.br>
2025-02-14 19:15:58 -0300

    atualizado GameState e adicionado Lens

--- app/GameState.hs
+++ app/GameState.hs
@@ -1,16 +1,18 @@
-module GameState (GameState(..), Selection(..), Turn(..), Board) where
-  
+{-# LANGUAGE TemplateHaskell #-}
+
+module GameState (GameState(..), Turn(..), cursor, selected, matrix, p1Count, p2Count, turn) where
+import Control.Lens
+
+
 data Turn = P1 | P2 deriving (Show, Eq)
 
-data Selection = NoSelection | Selected (Int, Int) deriving (Show, Eq)
-
-type Board = [[Int]]
-
 data GameState = GameState
-  { cursor    :: (Int, Int)  -- Posição do cursor na matriz.
-  , selected  :: Selection   -- Posição selecionada.
-  , matrix    :: Board       -- Representação do tabuleiro.
-  , p1Count   :: Int         -- Número de peças do jogador P1.
-  , p2Count   :: Int         -- Número de peças do jogador P2.
-  , turn      :: Turn        -- Indica de quem é a vez.
+  { _cursor    :: (Int, Int)      -- Posição do cursor na matriz.
+  , _selected  :: (Int, Int, Bool) -- Tupla com posição e booleano.
+  , _matrix    :: [[Int]]           -- Representação do tabuleiro.
+  , _p1Count   :: Int             -- Número de peças do jogador P1.
+  , _p2Count   :: Int             -- Número de peças do jogador P2.
+  , _turn      :: Turn            -- Indica de quem é a vez.
   } deriving (Show, Eq)
+
+makeLenses ''GameState
