ebc8102541874148f53ff447830ac1331ae8ea4e
Lenner Coutinho Pereira <lenner.coutinho.pereira@ccc.ufcg.edu.br>
2025-02-15 14:35:18 -0300

    Update HandleSelection.hs

--- app/HandleSelection.hs
+++ app/HandleSelection.hs
@@ -1,14 +1,15 @@
 module HandleSelection where
 
 import GameState
+import CellState
 import Control.Lens
 import Utils
 -- import MovimentByFinder
 
 checkSelection :: GameState -> Bool
 checkSelection state
-    | state ^. turn == P1 && checkBit 2 posMatrix == True = True
-    | state ^. turn == P2 && checkBit 3 posMatrix == True = True
+    | state ^. turn == P1 && checkBit bitP1 posMatrix == True = True
+    | state ^. turn == P2 && checkBit bitP2 posMatrix == True = True
     | otherwise = False
     where 
         cursorX = state ^. cursor ._1
@@ -23,10 +24,9 @@ handleSelection state
         cursorX = state ^. cursor . _1
         cursorY = state ^. cursor . _2
 
-        number = if state ^. turn == P1 then 4 else 8
         
-        stateAux = state & selected .~ (cursorX, cursorY, True)
-        newState = changeNumberInMatrix cursorX cursorY (number + 1) stateAux
+        newState = state & selected .~ (cursorX, cursorY, True)
+
         -- newState = movimentByFinder state2
     in
         newState
