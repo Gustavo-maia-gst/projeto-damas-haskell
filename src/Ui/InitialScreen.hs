68f8f23d369862b97250f1d580eb53ee990433df
Lenner Coutinho Pereira <lenner.coutinho.pereira@ccc.ufcg.edu.br>
2025-03-03 19:33:09 -0300

    centered text in howToPlay

--- src/Ui/InitialScreen.hs
+++ src/Ui/InitialScreen.hs
@@ -4,20 +4,21 @@ import CursesWrapper
 import UI.HSCurses.Curses
 import UI.HSCurses.CursesHelper
 import Control.Monad (when)
+import Data.List.Split (splitOn)
 
 printCentered :: String -> Int -> IO ()
 printCentered text y = do
     (height, width) <- scrSize 
     let x = (width - length text) `div` 2 
-    mvWAddStr stdScr y x text 
+    mvWAddStr stdScr y (max x 0) text 
 
-printTexts :: [String] -> Int -> Int -> IO ()
-printTexts [] _ _ = return ()
-printTexts (t:ts) indAt indCursor = do
+printTexts :: [String] -> Int -> Int -> Int -> IO ()
+printTexts [] _ _ _ = return ()
+printTexts (t:ts) indAt indCursor initLine = do
     if indAt == indCursor 
-        then printCentered ("-> " ++ t) (5 + 2 * indAt)
-        else printCentered t (5 + 2 * indAt)
-    printTexts ts (indAt + 1) indCursor
+        then printCentered ("-> " ++ t) (initLine + 2 * indAt)
+        else printCentered t (initLine + 2 * indAt)
+    printTexts ts (indAt + 1) indCursor initLine
 
 textHowToPlay :: String
 textHowToPlay = "Para se mover utilize as teclas W, A, S, D, ou as setas\nPara selecionar use a tecla ESPAÇO ou ENTER\nAo selecionar uma peça aparecerá os possiveis movimentos para ela, caso seja de múltiplos movimentos terá que fazer movimento por movimento\nAo virar dama poderá se mover para as 4 diagonais"
@@ -27,7 +28,7 @@ howPlay = do
     wclear stdScr
     
     (height, width) <- scrSize 
-    mvWAddStr stdScr 3 0 textHowToPlay
+    printTexts (splitOn "\n" textHowToPlay) 0 (-1) 3
     
     let text = "-> Voltar"
 
@@ -43,7 +44,7 @@ options :: Int -> IO Int
 options x = do
     wclear stdScr
     printCentered "Mova com W,S ou Up, Down. Para selecionar aperte ESPAÇO ou ENTER" 0
-    printTexts ["Contra outro jogador", "Contra Plinio (bot)", "Como jogar"] 0 x
+    printTexts ["Contra outro jogador", "Contra Plinio (bot)", "Como jogar"] 0 x 5
     refresh
     key <- getCh  
 
